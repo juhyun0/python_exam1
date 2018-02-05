ea5a3059a9f17d9258074974e62b88c35ceb6964
juhyun0 <36025326+juhyun0@users.noreply.github.com>
2018-02-05 18:29:51 +0900

    Add files via upload
    숫자->텍스트
    시프트 연산자
    연습문제

---
+++ exam.py
@@ -0,0 +1,99 @@
+print("1. 파이썬에서 변수란? ")
+print("데이터를 담는 메모리 공간")
+
+print("")
+
+print("2. 다음 코드가 출력하는 결과는 무엇입니까?")
+a=123+456
+print("a=123+456")
+print(a)
+
+print("")
+
+print("3. 다음 코드가 출력하는 결과는 무엇입니까?")
+a='123'+'456'
+print("a='123'+'456'")
+print(a)
+
+print("")
+
+print("4. 다음 코드 중 잘못된 것을 고르세요.")
+print("1) 123+456\n2) 1.23/4.56\n3) '123'*'456'")
+print("3")
+
+print("")
+
+print("5. 다음 코드가 출력하는 결과는 무엇입니까?")
+print("a=10\na-=5")
+a=10
+print(a)
+a-=5
+print(a)
+
+print("")
+
+print("6. 다음 코드가 출력하는 결과는 무엇입니까?")
+print("a='Good Morning'\n  a[:4]")
+a='Good Morning'
+print(a[:4])
+
+print("")
+
+print("7. 다음 코드가 출력하는 결과는 무엇입니까?")
+print("len='안녕하세요'")
+len='안녕하세요'
+print(len)
+
+print("")
+
+print("8.다음 코드가 출력하는 결과는 무엇입니까?")
+print("4>>2")
+print(4>>2)
+
+print("")
+
+print("9.다음 코드가 출력하는 결과는 무엇입니까?")
+print("4<<2")
+print(4<<2)
+
+print("")
+
+print("10.다음 코드가 출력하는 결과는 무엇입니까?")
+print("4|3")
+print(4|3)
+
+print("")
+
+print("11.다음 코드가 출력하는 결과는 무엇입니까?")
+print("4&3")
+print(4&3)
+
+print("")
+
+print("12.다음 코드에서 x는 10진수로 어떤 값을 갖게 될까요?")
+print("x=0b1001")
+x=0b1001
+print(x)
+
+print("")
+
+print("13.다음 코드에서 x는 10진수로 어떤 값을 갖게 될까요?")
+print("x=0xFF")
+x=0xFF
+print(x)
+
+print("")
+
+print("14.다음 코드에서 x는 16진수로 어떤 값을 갖게 될까요?")
+print("x=0xFF | 0x0F")
+x=0xFF | 0x0F
+print(x)
+
+print("")
+
+print("15. 다음 코드에서 a와 b는 각각 어떤 문자열을 갖게 될까요?")
+print("a='{0} {1}'.format('Mario',40)\nb='{name} {age}'.format(name='Luigi',age=35)")
+print('{0} {1}'.format('Mario',40))
+print('{name} {age}'.format(name='Luigi',age=35))
+
+
---
+++ input_multiply.py
@@ -0,0 +1,10 @@
+
+
+print("첫 번째 수를 입력하세요 : ")
+a=input() #숫자 입력
+print("두 번째 수를 입력하세요 : ")
+b=input()
+
+result=int(a)*int(b) 
+
+print("{0}*{1}={2}".format(a,b,result)) #숫자->문자
